8ede6f104ed13b3a2aaa8495d7f89a99e0be81e6
Julia Lisec <julialisec12@gmail.com>
2020-10-13 10:34:24 +0200

    set up day, month, year, and time for giveaway

--- app.js
+++ app.js
@@ -25,3 +25,17 @@ const weekdays = [
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('deadline-format h4');
+
+let futureDate = new Date(2020, 3, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+
+
+giveaway.textContent = `Giveaway ends on ${date} ${month} ${year} ${hours}:${minutes}`;
