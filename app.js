8134ea3007315ca002d7c357d70b16b45e320f9e
Julia Lisec <julialisec12@gmail.com>
2020-10-13 11:03:00 +0200

    select values for days, hours, minutes, seconds

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('deadline-format h4');
 
-let futureDate = new Date(2020, 3, 24, 11, 30, 0);
+let futureDate = new Date(2020, 10, 14, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -39,3 +39,33 @@ const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 
 giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60m
+  // 1d = 24hr
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  // calculate all values
+  let days = t / oneDay;
+  days = Math.floor(days);
+
+  let hours = Math.floor((t % oneDay) / oneHour);
+
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+
+  let second = Math.floor((t % oneMinute) / 1000);
+
+};
+
+getRemainingTime();
